f4a85e3541e8c879e3fd5182572be782f4c9ca44
Felix <loststars6517@icloud.com>
2024-04-13 20:00:27 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Felix Chan on 2024/4/13.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
